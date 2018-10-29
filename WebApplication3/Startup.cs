5e1ee864976c3138f4b579c3b472cbb5238f921e
Tuin Sarkar <tuhin.devl@outlook.com>
2018-10-29 07:56:20 +0000

    added missing files

---
+++ WebApplication3/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using WebApplication3.Data;
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ITitemQuery, TitemQuery>();
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver =
+                        new CamelCasePropertyNamesContractResolver();
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAPIResponseWrapperMiddleware();
+            //app.UseMiddleware(typeof(ErrorHandlingMiddleware));
+            app.UseMvc();
+        }
+    }
+}
---
+++ WebApplication3/my-db-script.sql
@@ -0,0 +1,6 @@
+﻿CREATE DATABASE MyDB101
+GO
+CREATE TABLE MyDB101.dbo.T1(
+	ID int identity primary key not null,
+	[Name] varchar(100)
+)
